6559c1ceefc9bb3ccdac9a0c6f4cf2e1595c92f3
grahamwren <graham@maineisland.co>
2019-01-17 18:21:34 -0500

    Initial calc impl

---
+++ 02/public/calc.js
@@ -0,0 +1,42 @@
+(function() {
+
+  function getHandleButtonClick(calc) {
+    return function (event) {
+      const button = event.target.getAttribute('data-button-val');
+      calc.dispatchEvent(button);
+    }
+  }
+
+  function handleUpdateDisplay({number, runningFunction, decimalMode}) {
+    const display = document.querySelector('.calc .display');
+    display.innerText = number;
+
+    const buttons = document.querySelectorAll('.calc .button');
+    buttons.forEach(e => e.classList.remove('listening'));
+    const runningFunctionButton =
+      Array.prototype.find.call(buttons, b => b.getAttribute('data-button-val') === runningFunction);
+    if (runningFunctionButton) {
+      runningFunctionButton.classList.add('listening');
+    }
+
+    console.log({decimalMode});
+    if (decimalMode) {
+      document.querySelector('.calc .button[data-button-val="."]').classList.add('listening');
+    } else {
+      document.querySelector('.calc .button[data-button-val="."]').classList.remove('listening');
+    }
+  }
+
+  function init() {
+    // create Calculator and connect display
+    const calculator = new Calculator(handleUpdateDisplay);
+
+    // connect button events
+    const buttons = document.querySelectorAll('.calc .button');
+    for (const button of buttons) {
+      button.addEventListener('click', getHandleButtonClick(calculator));
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+})();
---
+++ 02/public/javascript/calculator.js
@@ -0,0 +1,117 @@
+
+(function() {
+  class Calculator {
+    constructor(updateDisplayCallback = () => {}) {
+      this.listeners = new Map();
+      this._currentNumberBuffer = [];
+      this._runningFunction = null;
+      this._decimalMode = false;
+      this._resultMode = false;
+      this.updateDisplayCallback = updateDisplayCallback;
+
+      this.addListener('clear', this.clear.bind(this));
+      this.addListener('.', this.decimal.bind(this));
+
+      // Setup function listeners
+      Object.keys(Calculator.calculatorFeatures).forEach(name =>
+        this.addListener(name, this.createFn(name, Calculator.calculatorFeatures[name]))
+      );
+
+      // Setup num buffer listeners
+      for (let i = 0; i < 10; i++) {
+        this.addListener(i + '', () => this.num(i));
+      }
+    }
+
+    get currentNumber() {
+      if (this._currentNumberBuffer.length === 0) return;
+      return Number(this._currentNumberBuffer.join(''));
+    }
+    get runningFunction() { return this._runningFunction; }
+    get decimalMode() { return this._decimalMode; }
+
+    addListener(event, fn) {
+      const listeners = this.listeners.get(event) || [];
+      listeners.push(fn);
+      this.listeners.set(event, listeners);
+    }
+
+    dispatchEvent(event) {
+      const listeners = this.listeners.get(event);
+      if (listeners && listeners.length) listeners.forEach(fn => fn(event));
+    }
+
+    updateDisplay() {
+      this.updateDisplayCallback({
+        number: this.currentNumber || 0,
+        runningFunction: this.runningFunction && this.runningFunction.name,
+        decimalMode: this.decimalMode
+      });
+    }
+
+    evalRunning() {
+      if (!this.runningFunction) return;
+      const newNumber = this.runningFunction.eval(this.currentNumber);
+
+      // reset state to show result
+      this._currentNumberBuffer = String(newNumber).split('');
+    }
+
+    createFn(name, fn) {
+      return () => {
+        // if a new value has been added, eval prev closure with new value
+        if (!this._resultMode) this.evalRunning();
+
+        this._runningFunction = {
+          name,
+          eval: fn(this.currentNumber)
+        };
+        this._resultMode = true;
+        this._decimalMode = false;
+        this.updateDisplay();
+      }
+    }
+
+    clear() {
+      this._currentNumberBuffer.length = 0;
+      this._runningFunction = null;
+      this._decimalMode = false;
+      this._resultMode = false;
+
+      this.updateDisplay();
+    }
+
+    decimal() {
+      // if there is already a decimal, ignore
+      if (this.decimalMode) return;
+      this._decimalMode = true;
+
+      // if current number buffer is empty, insert a preceding '0' before '.'
+      if (this._currentNumberBuffer.length === 0) this.num('0');
+
+      this.num('.');
+      this.updateDisplay();
+    }
+
+    num(s) {
+      // if displaying a result, clear number before setting new number
+      if (this._resultMode) {
+        this._currentNumberBuffer.length = 0;
+        this._resultMode = false;
+      }
+      this._currentNumberBuffer = this._currentNumberBuffer || [];
+      this._currentNumberBuffer.push(s);
+
+      this.updateDisplay();
+    }
+  }
+
+  Calculator.calculatorFeatures = {
+    '+': (a, b) => b === undefined ? (c = 0) => a + c : a + b,
+    '-': (a, b) => b === undefined ? (c = 0) => a - c : a - b,
+    '*': (a, b) => b === undefined ? (c = 1) => a * c : a * b,
+    '/': (a, b) => b === undefined ? (c = 1) => a / c : a / b
+  };
+
+  window.Calculator = Calculator;
+})();
